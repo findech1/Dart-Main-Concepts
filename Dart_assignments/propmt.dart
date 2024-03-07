afb66ce66926f6469bf15a73584d51832977a91c
NdegeFelix <findech6@gmail.com>
2024-03-07 22:46:09 +0300

    completed assignment

---
+++ Dart_assignments/propmt.dart
@@ -0,0 +1,15 @@
+//Dart program that prompts the user for a number and then prints a message to the console
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter a number: ");
+  int number = int.parse(stdin.readLineSync()!);
+
+  if (number > 10) {
+    print("Your number is greater than 10");
+  } else if (number < 10) {
+    print("Your number is less than 10");
+  } else {
+    print("Your number is equal to 10");
+  }
+}
